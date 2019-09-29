7cb57be939f2115fb426bc0c6ad8ef4992739e88
Kevin B. Kwan Loo <kranok@gmail.com>
2019-09-28 22:06:48 -0500

    Add files via upload

---
+++ Form_Partitions_CSV.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+import random
+import os
+
+fileInText = "Data_sets/Data.txt"
+fileInTemplate = "Data_sets/Partition_Template_V2.csv"
+
+for partition in range(50,151):
+	count = 1
+	fileOut = "Partitions_csv_V1/Partition_"+str(partition)+".csv"
+	os.system("cp "+fileInTemplate+" "+fileOut)
+	outfile = open(fileOut, "a+")
+	classInstance = 0
+
+	for line in open(fileInText, "r"):
+		if(count > partition):
+			classInstance = 1
+		outfile.write(line[:line.find("\n")]+", "+str(classInstance)+"\n")
+		count += 1
+
+	outfile.close()
